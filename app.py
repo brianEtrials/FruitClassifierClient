a3a3834dd41f80b0e673cd8d242051830d096d55
Skyler Wiernik <skyler@Skylers-MacBook-Pro-3.local>
2024-07-28 16:13:49 -0700

    Fixed request bug

--- app.py
+++ app.py
@@ -2,7 +2,7 @@ from flask import Flask, render_template, request, jsonify
 import os
 import base64
 import pdb
-from flask_cors import CORS
+# from flask_cors import CORS
 
 app = Flask(__name__, template_folder='./templates')
 # CORS(app)
@@ -11,20 +11,20 @@ app = Flask(__name__, template_folder='./templates')
 # if not os.path.exists('./shots'):
 #     os.makedirs('./shots')
 
-# @app.route('/')
-# def index():
-#     return render_template('index.html')
+@app.route('/')
+def index():
+    return render_template('index.html')
 
 @app.route('/snapshot', methods=['POST'])
-def snapshot(request):
-    pdb.set_trace()
+def snapshot():
+    # pdb.set_trace()
     print(request)
-    # data = request.json
-    # image_data = base64.b64decode(data['image'].split(',')[1])
-    # file_path = f"./shots/fruit.jpg"
-    # with open(file_path, 'wb') as f:
-    #     f.write(image_data)
-    # return jsonify(status="success", file_path=file_path)
+    data = request.json
+    image_data = base64.b64decode(data['image'].split(',')[1])
+    file_path = f"./shots/fruit.jpg"
+    with open(file_path, 'wb') as f:
+        f.write(image_data)
+    return jsonify(status="success", file_path=file_path)
 
 if __name__ == '__main__':
     app.run(debug=True)
