99b5d81bd8ce5dc3bac580b3d71d9327d5417bfc
Brian Rojas <80777051+brianEtrials@users.noreply.github.com>
2024-07-28 19:03:16 -0400

    getting cors error

--- app.py
+++ app.py
@@ -1,28 +1,31 @@
 from flask import Flask, render_template, request, jsonify
 import os
 import base64
+import pdb
 from flask_cors import CORS
 
 app = Flask(__name__, template_folder='./templates')
-CORS(app)
+# CORS(app)
 
 # Create shots directory to save pictures
-if not os.path.exists('./shots'):
-    os.makedirs('./shots')
+# if not os.path.exists('./shots'):
+#     os.makedirs('./shots')
 
-@app.route('/')
-def index():
-    return render_template('index.html')
+# @app.route('/')
+# def index():
+#     return render_template('index.html')
 
 @app.route('/snapshot', methods=['POST'])
-def snapshot():
-    data = request.json
-    image_data = base64.b64decode(data['image'].split(',')[1])
-    file_path = f"./shots/fruit.jpg"
-    with open(file_path, 'wb') as f:
-        f.write(image_data)
-    return jsonify(status="success", file_path=file_path)
+def snapshot(request):
+    pdb.set_trace()
+    print(request)
+    # data = request.json
+    # image_data = base64.b64decode(data['image'].split(',')[1])
+    # file_path = f"./shots/fruit.jpg"
+    # with open(file_path, 'wb') as f:
+    #     f.write(image_data)
+    # return jsonify(status="success", file_path=file_path)
 
 if __name__ == '__main__':
-    app.run(debug=False)
+    app.run(debug=True)
 
